0b6da04be80bb7271ecd5a7fa89416bc9003126d
Hamza <Hamzasafdar125@gmail.com>
2024-08-09 16:56:39 +0500

    Almost There

---
+++ routes/nonVerbalQuizzes.routes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const nonVerbalQuiz = require('../controller/nonVerbalQuiz.controller')
+
+router.post('/createNonVerbalQuiz', nonVerbalQuiz.createNonVerbalQuiz);
+router.get('/viewNonVerbalQuizzes', nonVerbalQuiz.viewNonVerbalQuizzes);
+router.get('/viewNonVerbalQuiz/:title', nonVerbalQuiz.viewByTitle);
+router.get('/countNonVerbalQuizzes', nonVerbalQuiz.countNonVerbalQuizzes);
+router.delete('/deleteNonVerbalQuiz/:id', nonVerbalQuiz.deleteNonVerbalQuiz);
+router.patch('/updateNonVerbalQuiz/:id', nonVerbalQuiz.updateNonVerbalQuiz);
+module.exports = router;
