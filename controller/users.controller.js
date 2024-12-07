e924bfa14ef5fe86f472767188e04d714b355571
Hamza <Hamzasafdar125@gmail.com>
2024-12-08 03:15:24 +0500

    Login check

--- controller/users.controller.js
+++ controller/users.controller.js
@@ -59,9 +59,9 @@ exports.signup = async (req, res) => {
 
 exports.login = async (req, res) => {
   try {
-      const { email, password } = req.body;
-
-      const user = await User.findOne({ email });
+      const { email, password, role } = req.body;
+      console.log("Req body :", req.body)
+      const user = await User.findOne({ email, role });
       if (!user) {
           return res.status(401).json({ message: 'Invalid credentials' });
       }
