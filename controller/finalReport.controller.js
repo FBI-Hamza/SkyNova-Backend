bcc98920eab157af1a763e5fe91588c7e260b36a
Eisha313 <eishakamran313@gmail.com>
2024-12-10 11:46:04 +0500

    deletedddddddddd

--- controller/finalReport.controller.js
+++ controller/finalReport.controller.js
@@ -1,86 +1,87 @@
-const Report = require('../models/finalReport.model'); 
-const medicalDetailss = require('../models/medicalDetails.model');
-const VerbalQuizResultss = require('../models/verbalQuizResult.model'); 
-const nonVerbalQuizResultss = require('../models/nonVerbalQuizResult.model'); // Adjust the path as necessary
+const Report = require("../models/finalReport.model");
+const medicalDetailss = require("../models/medicalDetails.model");
+const VerbalQuizResultss = require("../models/verbalQuizResult.model");
+const nonVerbalQuizResultss = require("../models/nonVerbalQuizResult.model"); // Adjust the path as necessary
 
 const createReport = async (req, res) => {
   try {
-      const userId = req.user.userId; 
+    const userId = req.user.userId;
 
-      console.log('user ID', userId);
-      const medicalDetails = await medicalDetailss.findOne({userId});
-      console.log('medical details', medicalDetails);
-      const nonVerbalQuizResult = await nonVerbalQuizResultss.findOne({userId});
-      console.log('non verbal quiz result', nonVerbalQuizResult);
-      const verbalQuizResult = await VerbalQuizResultss.findOne({userId});
-      console.log('verbal quiz result', verbalQuizResult);
+    console.log("user ID", userId);
+    const medicalDetails = await medicalDetailss.findOne({ userId });
+    console.log("medical details", medicalDetails);
+    const nonVerbalQuizResult = await nonVerbalQuizResultss.findOne({ userId });
+    console.log("non verbal quiz result", nonVerbalQuizResult);
+    const verbalQuizResult = await VerbalQuizResultss.findOne({ userId });
+    console.log("verbal quiz result", verbalQuizResult);
 
-      if (!medicalDetails || !nonVerbalQuizResult || !verbalQuizResult) {
-        return res.status(404).json({ error: 'Not Found' });
-      }
+    if (!medicalDetails || !nonVerbalQuizResult || !verbalQuizResult) {
+      return res.status(404).json({ error: "Not Found" });
+    }
 
     const newReport = new Report({
+      userId,
       nonVerbalQuizResult,
       verbalQuizResult,
       medicalDetails,
     });
 
     if (!medicalDetails || !nonVerbalQuizResult || !verbalQuizResult) {
-      return res.status(404).json({ error: 'Not Found' });
+      return res.status(404).json({ error: "Not Found" });
     }
 
     await newReport.save();
-    return res.status(201).json({ message: 'Report created successfully', report: newReport });
+    return res.status(201).json({ message: "Report created successfully", report: newReport });
   } catch (error) {
-    console.error('Error creating report:', error);
-    return res.status(500).json({ error: 'Internal Server Error' });
+    console.error("Error creating report:", error);
+    return res.status(500).json({ error: "Internal Server Error" });
   }
 };
 
 const viewReports = async (req, res) => {
   try {
     const reports = await Report.find({})
-      .populate('nonVerbalQuizResult')
-      .populate('verbalQuizResult')
-      .populate('medicalDetails');
+      .populate("nonVerbalQuizResult")
+      .populate("verbalQuizResult")
+      .populate("medicalDetails");
 
-    res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
-    res.setHeader('Pragma', 'no-cache');
-    res.setHeader('Expires', 0);
+    res.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+    res.setHeader("Pragma", "no-cache");
+    res.setHeader("Expires", 0);
     return res.json(reports);
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: 'Server error' });
+    res.status(500).json({ message: "Server error" });
   }
 };
 
 const reportViewById = async (req, res) => {
   try {
     const report = await Report.findById(req.params.id)
-      .populate('nonVerbalQuizResult')
-      .populate('verbalQuizResult')
-      .populate('medicalDetails');
+      .populate("nonVerbalQuizResult")
+      .populate("verbalQuizResult")
+      .populate("medicalDetails");
 
     if (!report) {
-      return res.status(404).json({ message: 'Report not found' });
+      return res.status(404).json({ message: "Report not found" });
     }
     return res.json(report);
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: 'Server error' });
+    res.status(500).json({ message: "Server error" });
   }
 };
 
 const countReports = async (req, res) => {
   try {
     const reportCount = await Report.countDocuments({});
-    res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
-    res.setHeader('Pragma', 'no-cache');
-    res.setHeader('Expires', 0);
+    res.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+    res.setHeader("Pragma", "no-cache");
+    res.setHeader("Expires", 0);
     return res.json({ reportCount });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: 'Server error' });
+    res.status(500).json({ message: "Server error" });
   }
 };
 
@@ -91,13 +92,13 @@ const deleteReport = async (req, res) => {
     const deletedReport = await Report.deleteOne({ _id: reportId });
 
     if (deletedReport.deletedCount === 0) {
-      return res.status(404).json({ message: 'Report not found' });
+      return res.status(404).json({ message: "Report not found" });
     }
 
-    return res.json({ message: 'Report deleted successfully' });
+    return res.json({ message: "Report deleted successfully" });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: 'Server error' });
+    res.status(500).json({ message: "Server error" });
   }
 };
 
@@ -109,13 +110,13 @@ const updateReport = async (req, res) => {
     const updatedReport = await Report.findByIdAndUpdate(reportId, { $set: updatedData }, { new: true });
 
     if (!updatedReport) {
-      return res.status(404).json({ message: 'Report not found' });
+      return res.status(404).json({ message: "Report not found" });
     }
 
     return res.json(updatedReport);
   } catch (error) {
-    console.error('Error updating report:', error);
-    res.status(500).json({ message: 'Internal server error' });
+    console.error("Error updating report:", error);
+    res.status(500).json({ message: "Internal server error" });
   }
 };
 
--- models/finalReport.model.js
+++ models/finalReport.model.js
@@ -1,25 +1,30 @@
 const mongoose = require("mongoose");
 
 const finalReportSchema = mongoose.Schema({
-    nonVerbalQuizResult: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'nonVerbalQuizResult', 
-        required: true 
-    },
-    verbalQuizResult: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'VerbalQuizResult', 
-        required: true 
-    },
-    medicalDetails: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Medical Details',
-        required: true 
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now
-    }
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  nonVerbalQuizResult: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "nonVerbalQuizResult",
+    required: true,
+  },
+  verbalQuizResult: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "VerbalQuizResult",
+    required: true,
+  },
+  medicalDetails: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Medical Details",
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
-module.exports = mongoose.model('Final Report', finalReportSchema);
+module.exports = mongoose.model("Final Report", finalReportSchema);
